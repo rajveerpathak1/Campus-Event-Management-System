f9f23cae76ac992015a3441ae1c70069701ebf43
Rajveer Pathak <tarupa123@gmail.com>
2026-01-14 00:35:00 +0530

    coded some routes in eventRoutes.js and used them in app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const session = require("./config/session");
 
+const event = require('./routes/eventRoutes');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session);
+
+app.use('/events', event);
+
+
+app.get("/",(req,res)=>{
+console.log("someone hit on the server !!!!!");
+res.status(200).send("Welcome to our event management system !!!");
+});
+
 
 
 module.exports = app;
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../config/db");
+const isauth = require("../middlewares/authMiddleware.js")
 
 
-
-
+router.get("/",isauth ,async (req, res) => {
+  try {
+    const result = await pool.query("SELECT title FROM events");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Database error" });
+  }
+});
 
 module.exports = router;
