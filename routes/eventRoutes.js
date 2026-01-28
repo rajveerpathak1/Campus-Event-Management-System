085024f3c648c50083ba5693263b50164f03b640
Rajveer Pathak <tarupa123@gmail.com>
2026-01-28 23:58:20 +0530

    completed admin route

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const {
   getEvents,
   getEvent,
+//  register
 } = require("../controllers/eventController");
 
 const router = express.Router();
 
 router.get("/", getEvents);
 router.get("/:id", getEvent);
-
+//router.get("/:id/register",register);
 module.exports = router;
