e73524343a8d93242b7864d568e649f4db7fb134
Rajveer Pathak <tarupa123@gmail.com>
2026-04-12 00:32:20 +0530

    updated session settings

--- config/session.js
+++ config/session.js
@@ -31,8 +31,8 @@ const createSessionMiddleware = () => {
 
     cookie: {
       httpOnly: true,
-      secure: isProd, // ⚠ requires HTTPS in prod
-      sameSite: isProd ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
   });
